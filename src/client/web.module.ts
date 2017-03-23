39600fc19dcdebd7f96fd747dc2c56dda94a4808
Amir Toole <amir.toole@caseware.com>
2017-03-22 18:08:17 -0600

    fontawesome integration

--- src/client/web.module.ts
+++ src/client/web.module.ts
@@ -1,31 +1,32 @@
 // angular
-import { NgModule } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { Http } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {Http} from '@angular/http';
 
 // libs
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { TranslateLoader } from '@ngx-translate/core';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {TranslateLoader} from '@ngx-translate/core';
+import {Angular2FontawesomeModule} from 'angular2-fontawesome';
 
 // app
-import { APP_COMPONENTS, AppComponent } from './app/components/index';
-import { routes } from './app/components/app.routes';
+import {APP_COMPONENTS, AppComponent} from './app/components/index';
+import {routes} from './app/components/app.routes';
 
 // feature modules
-import { CoreModule } from './app/shared/core/core.module';
-import { AppReducer } from './app/shared/ngrx/index';
-import { AnalyticsModule } from './app/shared/analytics/analytics.module';
-import { MultilingualModule, translateLoaderFactory } from './app/shared/i18n/multilingual.module';
-import { MultilingualEffects } from './app/shared/i18n/index';
-import { SampleModule } from './app/shared/sample/sample.module';
-import { NameListEffects } from './app/shared/sample/index';
+import {CoreModule} from './app/shared/core/core.module';
+import {AppReducer} from './app/shared/ngrx/index';
+import {AnalyticsModule} from './app/shared/analytics/analytics.module';
+import {MultilingualModule, translateLoaderFactory} from './app/shared/i18n/multilingual.module';
+import {MultilingualEffects} from './app/shared/i18n/index';
+import {SampleModule} from './app/shared/sample/sample.module';
+import {NameListEffects} from './app/shared/sample/index';
 
 // config
-import { Config, WindowService, ConsoleService } from './app/shared/core/index';
+import {Config, WindowService, ConsoleService} from './app/shared/core/index';
 Config.PLATFORM_TARGET = Config.PLATFORMS.WEB;
 if (String('<%= BUILD_TYPE %>') === 'dev') {
   // only output console logging in dev mode
@@ -33,8 +34,8 @@ if (String('<%= BUILD_TYPE %>') === 'dev') {
 }
 
 // sample config (extra)
-import { AppConfig } from './app/shared/sample/services/app-config';
-import { MultilingualService } from './app/shared/i18n/services/multilingual.service';
+import {AppConfig} from './app/shared/sample/services/app-config';
+import {MultilingualService} from './app/shared/i18n/services/multilingual.service';
 // custom i18n language support
 MultilingualService.SUPPORTED_LANGUAGES = AppConfig.SUPPORTED_LANGUAGES;
 
@@ -69,8 +70,8 @@ if (String('<%= BUILD_TYPE %>') === 'dev') {
   imports: [
     BrowserModule,
     CoreModule.forRoot([
-      { provide: WindowService, useFactory: (win) },
-      { provide: ConsoleService, useFactory: (cons) }
+      {provide: WindowService, useFactory: (win)},
+      {provide: ConsoleService, useFactory: (cons)}
     ]),
     routerModule,
     AnalyticsModule,
@@ -81,9 +82,10 @@ if (String('<%= BUILD_TYPE %>') === 'dev') {
     }]),
     SampleModule,
     StoreModule.provideStore(AppReducer),
-DEV_IMPORTS,
+    DEV_IMPORTS,
     EffectsModule.run(MultilingualEffects),
-    EffectsModule.run(NameListEffects)
+    EffectsModule.run(NameListEffects),
+    Angular2FontawesomeModule
   ],
   declarations: [
     APP_COMPONENTS
@@ -97,4 +99,5 @@ DEV_IMPORTS,
   bootstrap: [AppComponent]
 })
 
-export class WebModule { }
+export class WebModule {
+}
--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,6 +1,6 @@
-import { join } from 'path';
-import { SeedAdvancedConfig } from './seed-advanced.config';
-// import { ExtendPackages } from './seed.config.interfaces';
+import {join} from 'path';
+import {SeedAdvancedConfig} from './seed-advanced.config';
+import { ExtendPackages } from './seed.config.interfaces';
 
 /**
  * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
@@ -9,7 +9,7 @@ import { SeedAdvancedConfig } from './seed-advanced.config';
 export class ProjectConfig extends SeedAdvancedConfig {
 
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
-  
+
   constructor() {
     super();
     // this.APP_TITLE = 'Put name of your app here';
@@ -30,15 +30,11 @@ export class ProjectConfig extends SeedAdvancedConfig {
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
 
-    // Add packages (e.g. ng2-translate)
-    // ng2-translate is already added with the advanced seed - here for example only
-    // let additionalPackages: ExtendPackages[] = [{
-    //   name: 'ng2-translate',
-    //   // Path to the package's bundle
-    //   path: 'node_modules/ng2-translate/bundles/ng2-translate.umd.js'
-    // }];
-    //
-    // this.addPackagesBundles(additionalPackages);
+    let additionalPackages: ExtendPackages[] = [{
+      name: 'angular2-fontawesome',
+      path: 'node_modules/angular2-fontawesome/angular2-fontawesome.js'
+    }];
+    this.addPackagesBundles(additionalPackages);
 
     /* Add to or override NPM module configurations: */
     // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
